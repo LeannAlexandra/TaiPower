93d24a0c6a9c3919b427adb72a50b4191e03c875
LeannAlexandra <LeannAlexandraViolet@gmail.com>
2022-07-21 20:39:18 +0200

    TaiPower Version 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,16 +33,30 @@ namespace TaiPower
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelText = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBarWaitTime = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWaitTime)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(254, 107);
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::TaiPower.Properties.Resources.Tai_Power_Button;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(107, 209);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(105, 64);
             this.button1.TabIndex = 0;
-            this.button1.Text = "TESTING";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Accept";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // notifyIcon1
@@ -50,18 +64,105 @@ namespace TaiPower
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "TaiPower";
             this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.BackColor = System.Drawing.Color.Transparent;
+            this.labelTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelTitle.Location = new System.Drawing.Point(291, 260);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(119, 13);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Resulution 1920 x 1080";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.BackColor = System.Drawing.Color.Transparent;
+            this.labelText.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.labelText.Location = new System.Drawing.Point(116, 190);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(82, 13);
+            this.labelText.TabIndex = 2;
+            this.labelText.Text = "Idle Time: 0 sec";
+            this.labelText.Click += new System.EventHandler(this.labelText_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::TaiPower.Properties.Resources.Tai_Power_Button;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(251, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 64);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBarWaitTime
+            // 
+            this.trackBarWaitTime.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.trackBarWaitTime.LargeChange = 30;
+            this.trackBarWaitTime.Location = new System.Drawing.Point(134, 158);
+            this.trackBarWaitTime.Maximum = 300;
+            this.trackBarWaitTime.Minimum = 30;
+            this.trackBarWaitTime.Name = "trackBarWaitTime";
+            this.trackBarWaitTime.Size = new System.Drawing.Size(259, 45);
+            this.trackBarWaitTime.TabIndex = 4;
+            this.trackBarWaitTime.TickFrequency = 10;
+            this.trackBarWaitTime.Value = 30;
+            this.trackBarWaitTime.Scroll += new System.EventHandler(this.trackBarWaitTime_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(248, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Start After: 5 mins";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::TaiPower.Properties.Resources.Tai_PowerYES;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(456, 327);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBarWaitTime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelTitle);
             this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "TAI-POWER!";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWaitTime)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +170,11 @@ namespace TaiPower
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBarWaitTime;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,66 +1,119 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 //using Utilities;
 
 namespace TaiPower
 {
     public partial class Form1 : Form
     {
-        private bool f12toggle = true;
-       Cursor cursor;
+        private bool appRunning = false;
+        private bool quit = false;
+        int screenWidth;
+        int screenHeight;
+        //float aspectRatio;
+        Thread thread;
+        GetLastUserInput gi;
+        int waittime = 300000;//default 5 mins
+        int buffer = 20;// buffer from the edge bottom of the screen.
+        int taskbarbuffer = 10;
+
+       // Cursor cursor;
+       private void SaveToStartup()
+        {
+            //test if it already exist
+            //then  save there
+
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            try
+            {
+                reg.GetValue("TaiPower", Application.ExecutablePath.ToString());
+                //if(reg.TaiPowe)
+                //Debug.WriteLine("ITS WRITTEN IN THE WIND: "+reg.GetValue("TaiPower"));
+               // reg.SetValue("TaiPower", Application.ExecutablePath.ToString());
+                
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+                /*
+            string[] registryList = reg.GetSubKeyNames();
+
+            foreach (string registryEntry in registryList)
+            {
+                Debug.WriteLine(registryEntry);
+                //reg.SetValue("TaiPower", Application.ExecutablePath.ToString());
+
+            }
+//            reg.SetValue("TaiPower",Application.ExecutablePath.ToString());
+            */
+            //MessageBox.Show("Tai Power is now a part of your system forever","AWESOME NEWS",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+        }
         public Form1()
         {
             InitializeComponent();
-            cursor = new Cursor(Cursor.Current.Handle);
+            this.WindowState = FormWindowState.Minimized;
+            
+            gi = new GetLastUserInput();
+            thread = new Thread(new ThreadStart(WorkThreadFunction));
+            // cursor = new Cursor(Cursor.Current.Handle);
             this.ShowInTaskbar = false;//hide from task bar. 
-            //this.Visible = false; //this doens't do as intended, but ey, it's here and not create any problems.
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; //take away the border and closable to0lbar
+            this.Visible = false; //this doens't do as intended, but ey, it's here and not create any problems.
+            //this.FormBorderStyle = FormBorderStyle.None; //take away the border and closable to0lbar
             this.ShowInTaskbar = false;//hide from task bar. 
-            //this.WindowState = FormWindowState.Minimized; //minimize the Infosheet*
-            Thread thread = new Thread(new ThreadStart(WorkThreadFunction));
+            this.WindowState = FormWindowState.Normal; //minimize the Infosheet
+                                                       //
+            this.trackBarWaitTime.Value = waittime/1000;
+            //this.trackBarWaitTime.Enabled = true;
+            label4.Text = "Current Setting: 5 mins";
+            //thread = new Thread(new ThreadStart(WorkThreadFunction));
             //thread.
             //Debug.WriteLine("Thread Made");
-            thread.Start();
-        }
-        private void MoveCursor()
-        {
-            // Set the Current cursor, move the cursor's Position,
-            // and set its clipping rectangle to the form. 
+            this.TopMost = true;
+            
+            //this.Visible = false;
+            //this.WindowState = FormWindowState.Minimized;
+            screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            screenHeight = Screen.PrimaryScreen.Bounds.Height;
 
-            this.Cursor = new Cursor(Cursor.Current.Handle);
-            //Cursor.Current = Cursors.WaitCursor;
-            RelativeMove(Cursor.Position.X - 10, Cursor.Position.Y - 10);
-            //Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
-            //button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            //Cursor.Clip = new Rectangle(this.Location, this.Size);
+
+
+            this.Location = new Point(screenWidth - this.Width -buffer, screenHeight-this.Height-buffer-taskbarbuffer);
+            //this.Location = new Point(screenWidth-500, screenHeight-500);
+            
+            
+            
+            labelTitle.Text = ("Resolution: " + screenWidth + "x" + screenHeight);
+            Thread.Sleep(100);
+            thread.Start();
+
+            ////this.WindowState = FormWindowState.Minimized;
+            ///
+            SaveToStartup();
+            Thread.Sleep(1000);
+            buttonClicked();
         }
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        
+        [DllImport("user32.dll")]
         static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
         public static void RelativeMove(int relx, int rely)
         {
             mouse_event(0x0001, relx, rely, 0, 0);
+            //mouse_event()
                 
         }
-
-        //then to move mouse Relativemove(whereeveruwantx, y);
-
-        private void ChangePosition() 
-            // This is used to change the trajectory once one/ or both values reach 0
-        { 
-        
-        
-        
-        }
-
          protected override CreateParams CreateParams
         {
             get
@@ -71,54 +124,255 @@ namespace TaiPower
                 return cp;
             }
         }
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        private void buttonClicked()
         {
-            if (e.KeyCode == Keys.F12)
-            {
-                //DisplayText, Asuming you have a textbox do something like this:
-                Debug.WriteLine("The power of f12 activated");
-            }
+            //appRunning = !appRunning;
+            this.Visible = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.Visible = false;
+            this.TopMost = false;
         }
-
-
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+        /*
+         [DllImport("user32.dll")]
+         static extern IntPtr SetWindowsHookEx(int idHook, keyboardHookCallback callback, IntPtr hInstance, uint threadId);
+        */
 
+        //public static int IdleTime() //In seconds
+        //{
+        //    LASTINPUTINFO lastinputinfo = new LASTINPUTINFO();
+        //    lastinputinfo.cbSize = Marshal.SizeOf(lastinputinfo);
+        //    GetLastInputInfo(ref lastinputinfo);
+        //    return (((Environment.TickCount & int.MaxValue) - (lastinputinfo.dwTime & int.MaxValue)) & int.MaxValue) / 1000;
+        //}
         private void button1_Click(object sender, EventArgs e)
         {
-            //MoveCursor();
-            //Debug.WriteLine("Clicked");
-            
-            //Debug.WriteLine("fret started");
+            buttonClicked();
         }
+        //public static void tickCount()
+        //{
+        //    //you can do what you want. but not set something outside of the thread.   
+        //    //Form1.labelText.Text = "TIME IIS TICKING: " + GetLastUserInput.GetIdleTickCount();
+        //}
+        // "boring Spaceholdertext " + i;
         public void WorkThreadFunction()
         {
-            //Debug.WriteLine("thret 1");
-            try
-            {
-                Debug.WriteLine("thret try");
+            Thread.Sleep(1000);//one second start delay (allow app to load.)
+            bool right = true;
+            bool down = true;
+            int dirX = 1;
+            int dirY = 1;
+            int x = -1;
+            int y = -1;
+            //this is hardcoded (because it's one use program only...)
+            //index data
+            //0     current (old x position)
+            //1     current (old y position)
+            //2     new (expected x position)
+            //3     new (expected y position)
 
-                // do any background work
-                if (f12toggle)
-                {
-                    Debug.WriteLine("f12 true");
-                    for (int i = 0; i < 10; i++) { 
-                    Thread.Sleep(1000);
-                    MoveCursor(); 
-                }
-                } /*else
-                {
-                    Debug.WriteLine("f12 true");
-                }*/
-            }
-            catch (Exception ex)
+            //Label l = Form1.;   
+            ///TODO: 
+            /// constantly test if the user was inactive for more than 30 seconds if it is, turn on appRunning)
+            /// 
+            //Debug.WriteLine("thret 1");
+            do
             {
-                Debug.WriteLine("ERROR!!!: " + ex.Message);
-                // log errors
+                //int i = 0; //this is internal counter for this thread.
+                while (!appRunning)
+                {
+
+                    string settingUpdate = "Idle Time: ";
+                    int uhm = (int)GetLastUserInput.GetIdleTickCount() / 1000;
+                    int mins = (int)(uhm/ 60);
+                    int seconds = (int)(uhm % 60);
+
+                    if (mins != 0)
+                        settingUpdate += mins + " Min ";
+                    if (seconds != 0)
+                        settingUpdate += seconds + " Sec";
+
+                    labelText.Invoke((MethodInvoker)(() =>
+                    {
+                        labelText.Text = settingUpdate;
+                    }));
+                    if (GetLastUserInput.GetIdleTickCount() >= waittime*1000)
+                    {
+
+                        appRunning = !appRunning;
+                        //set starting position.
+                        x = Cursor.Position.X;
+                        y = Cursor.Position.Y;
+                        //set the start position.-
+                        //calculate the next position
+                    }
+                    
+                    
+                    ////?##################THIS CAN BE DELETED>>>>????????
+                    //TODO: Test for inactivity 
+                    //Debug.WriteLine(GetLastUserInput.GetIdleTickCount());
+                    //tickCount();
+                    Thread.Sleep(1000);
+                    /////######################### EDN OF MARKED FOR DELETION
+                }
+                //when the go ahead is given (please find a way out of this loop via TESTING for alternative user input)
+                while (appRunning)
+                {
+                    try
+                    {
+                        Thread.Sleep(5);
+                        if (x==Cursor.Position.X && y == Cursor.Position.Y) // (ie, the user has not moved the mouse after starting
+                        {
+
+                            
+                            
+                            Form1.RelativeMove(dirX, dirY); //set position
+                            
+                            x = Cursor.Position.X;
+                            y = Cursor.Position.Y;
+
+
+
+                           if ((x == 0 || x == screenWidth - 1) && (y == screenHeight - 1 || y == 0))
+                                Debug.WriteLine($"WINNER WINNER CHICKEN DINNER: {x},{y}"); //fun corner win*
+                          
+                            if (x == (screenWidth - 1)&&right)
+                            {
+                                dirX *= -1;
+                                right = !right;
+                            }
+                            if (x == 0 && !right)
+                            {
+                                dirX *= -1;
+                                right = !right;
+                            }
+                            if (y == (screenHeight - 1)&&down)
+                            {
+                                dirY *= -1;
+                                down = !down;
+                            }
+                            if (y == 0 && !down)
+                            {
+                                dirY *= -1;
+                                down = !down;
+                            }
+                            //if (i > 100)
+                            //{
+                            //    dirX *= -1;
+                            //    dirY*= -1;
+                            //    i = 0;
+                            //}
+                        }
+                        else
+                        {
+                            appRunning = !appRunning;//the program reset to wait for inactive time
+                        }
+
+
+                        
+                        //labelText.Invoke((MethodInvoker)(() =>
+                        //{
+                        //    labelText.Text = $"The App is working perfectly well, \n {x},{y}";
+                        //}));
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("ERROR!!!: " + ex.Message);
+
+                        // log errors
+                        break;
+                    }
+                    //Debug.WriteLine(GetLastUserInput.GetIdleTickCount());
+                }
+            } while (!quit); //infinitely.
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.Visible = true;
+            this.TopMost = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            thread.Abort();
+            Application.Exit();
+        }
+
+        private void trackBarWaitTime_Scroll(object sender, EventArgs e)
+        {
+            string settingUpdate = "Start after: ";
+            int mins = (int)this.trackBarWaitTime.Value / 60;
+            int seconds = (int)this.trackBarWaitTime.Value % 60;
+
+            if (mins != 0)
+                settingUpdate += mins + " Min ";
+            if (seconds != 0)
+                settingUpdate += seconds + " Sec";
+            
+            label4.Text= settingUpdate;
+
+            waittime = (int)this.trackBarWaitTime.Value;
+            //Debug.WriteLine($"WAIT TIME NOW: {waittime}");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class GetLastUserInput
+    {
+        private struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+        private static LASTINPUTINFO lastInPutNfo;
+        static GetLastUserInput()
+        {
+            lastInPutNfo = new LASTINPUTINFO();
+            lastInPutNfo.cbSize = (uint)Marshal.SizeOf(lastInPutNfo);
+        }
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        /// <summary>
+        /// Idle time in ticks
+        /// </summary>
+        /// <returns></returns>
+        public static uint GetIdleTickCount()
+        {
+            return ((uint)Environment.TickCount - GetLastInputTime());
+        }
+        /// <summary>
+        /// Last input time in ticks
+        /// </summary>
+        /// <returns></returns>
+        public static uint GetLastInputTime()
+        {
+            if (!GetLastInputInfo(ref lastInPutNfo))
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
             }
+            return lastInPutNfo.dwTime;
         }
     }
 }
